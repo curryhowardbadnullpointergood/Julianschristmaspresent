aaccd53dfd7ac4bc1d0feaa90f5f8aa077357253
fzrdi <ferdi.ibrahim@hotmail.com>
2024-04-18 14:15:58 +0100

    small commit, playing with just grabbing data with josh's gqleval as the simple question thing

--- TestThing.hs
+++ TestThing.hs
@@ -4,6 +4,9 @@ import System.IO
 import DataLexer
 import DataParser
 import GqlEval
+import Data.List
+
+-- THIS WHOLE FUNCTION IS JSUT TO HELP ME UNDERSTAND HOW EVERYTHING THUS FAR WORKS.
 
 handleIOException :: IOException -> IO ()
 handleIOException e = hPutStrLn stderr ("IO Error: " ++ show e)
@@ -28,13 +31,37 @@ processFile fileName = do
 evaluateAndPrintResults :: File -> IO ()
 evaluateAndPrintResults file = do
     putStrLn "Printing all ID values from the file:"
-    print $ GqlEval.returnIDValues file
+    print $ returnIDValues file
+
+    putStrLn "\nIDs eligible based on age <= 25 or label 'Visitor':"
+    let eligibleIDs = filterEligibleNodes file
+    print eligibleIDs
+
     putStrLn "\nEvaluating and printing node records for specific IDs:"
     mapM_ (printNodeRecord file) ["rw5", "jj23", "nonexistentID"]
 
 printNodeRecord :: File -> String -> IO ()
 printNodeRecord file nodeId = do
-    let result = GqlEval.returnNodeRecord file nodeId
+    let result = returnNodeRecord file nodeId
     case result of
-        Just node -> putStrLn $ "Record for " ++ nodeId ++ ": " ++ GqlEval.printNodeEntry node
+        Just node -> putStrLn $ "Record for " ++ nodeId ++ ": " ++ printNodeEntry node
         Nothing -> putStrLn $ "No record found for ID: " ++ nodeId
+
+filterEligibleNodes :: File -> [String]
+filterEligibleNodes (File nodeSets _) = concatMap filterNodeSet nodeSets
+
+filterNodeSet :: NodeSet -> [String]
+filterNodeSet (NodeSet _ nodeEntries) = map getNodeID $ filter isEligible nodeEntries
+
+isEligible :: NodeEntry -> Bool
+isEligible (NodeEntry _ literals labels) =
+    let hasEligibleAge = any (\lit -> case lit of
+                                LiteralInt age -> age <= 25
+                                _ -> False) literals
+        isVisitor = any (\label -> case label of
+                                Label "Visitor" -> True
+                                _ -> False) labels
+    in hasEligibleAge || isVisitor
+
+getNodeID :: NodeEntry -> String
+getNodeID (NodeEntry id _ _) = id
