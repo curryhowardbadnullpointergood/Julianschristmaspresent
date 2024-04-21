0780158c5b9cb1e3e0f22f16a14d7b23bbaecd47
fzrdi <ferdi.ibrahim@hotmail.com>
2024-04-21 15:18:04 +0100

    testing creating new Nodes

--- GqlLibrary.hs
+++ GqlLibrary.hs
@@ -4,14 +4,74 @@ import InputParser
 
 import Data.Maybe
 import Control.Monad
+import Data.Char
+import Data.Maybe
+import Text.Read
 import Data.List
-import System.Environment ( getArgs )
+import Data.Char
+import System.Environment 
 import Control.Exception
 import System.IO
 
 ---------------------------------------------------------------------------------------------------
 -- Being Currently Tested
 ---------------------------------------------------------------------------------------------------
+
+-- Function to combine any number of lists of strings into a list of lists of strings
+combineListsDynamically :: [[String]] -> [[String]]
+combineListsDynamically lists = transpose lists
+
+parseHeader :: String -> Bool -> NodeHeader
+parseHeader header hasLabel =
+    let fields = map parseField $ mySplit ',' $ trim header
+    in NodeHeader (filter (\(Field name _) -> name /= ":LABEL") fields) hasLabel
+  where
+    parseField :: String -> Field
+    parseField field = 
+        let (fieldName, fieldType) = break (== ':') field
+            typeName = trim $ drop 1 fieldType
+        in case typeName of
+            "integer" -> Field (trim fieldName) TypeInteger
+            "string"  -> Field (trim fieldName) TypeString
+            "boolean"    -> Field (trim fieldName) TypeBoolean
+            _         -> Field (trim fieldName) TypeString  -- Default to string if unspecified
+    trim :: String -> String
+    trim = f . f
+        where f = reverse . dropWhile isSpace
+    mySplit :: Char -> String -> [String]
+    mySplit delim str = case break (== delim) str of
+        (a, b) -> a : case b of
+            [] -> []
+            (_:b') -> mySplit delim b'
+
+
+
+-- Improved function to parse header and convert lists of strings into Nodes
+convertToNodes :: String -> Bool -> [[String]] -> Nodes
+convertToNodes header hasLabel dataList =
+    let NodeHeader fields _ = parseHeader header hasLabel
+    in map (createNode (NodeHeader fields hasLabel)) dataList
+
+-- Function to create a Node from a list of strings, now handling labels
+createNode :: NodeHeader -> [String] -> Node
+createNode header@(NodeHeader fields hasLabel) values =
+    let (literalsData, labelsData) = if hasLabel then (init values, last values) else (values, "")
+        literals = zipWith toLiteral fields literalsData
+        labels = if hasLabel then [Label labelsData] else []
+    in (header, NodeEntry (head literalsData) literals labels)
+
+-- Convert string value to Literal based on field type
+toLiteral :: Field -> String -> Literal
+toLiteral (Field _ TypeInteger) value = LiteralInt (fromMaybe 0 (readMaybe value))
+toLiteral (Field _ TypeString) value = LiteralStr value
+toLiteral (Field _ TypeBoolean) value = LiteralBool (fromMaybe False (readMaybe value))
+
+
+
+
+
+
+
 
 extractNodeIDs :: Nodes -> [String]
 extractNodeIDs nodes = map (\(_, NodeEntry id _ _) -> id) nodes
@@ -29,6 +89,40 @@ extractRelationEndIDs relationships = map (\(_, RelationshipEntry _ _ endId _) -
 
 extractRelationTypes :: Relations -> [String]
 extractRelationTypes relationships = map (\(_, RelationshipEntry _ _ _ typeLabel) -> typeLabel) relationships
+
+extractNodeFields :: String -> Nodes -> [String]
+extractNodeFields fieldName nodes = concatMap extractField nodes
+  where
+    extractField :: Node -> [String]
+    extractField (NodeHeader fields _, NodeEntry _ literals _) =
+      case findIndex (\(Field name _) -> name == fieldName) fields of
+        Just idx -> case safeGet idx literals of
+                      Just (LiteralInt age) -> [show age]  -- Convert Int to String
+                      _ -> []
+        Nothing -> []
+    safeGet :: Int -> [a] -> Maybe a
+    safeGet idx xs
+      | idx < length xs = Just (xs !! idx)
+      | otherwise = Nothing
+
+extractRelationFields :: String -> Relations -> [String]
+extractRelationFields fieldName relations = concatMap extractField relations
+  where
+    extractField :: Relation -> [String]
+    extractField (RelationshipHeader fields, RelationshipEntry startId literals endId typeLabel) =
+      case findIndex (\(Field name _) -> name == fieldName) fields of
+        Just idx -> case safeGet idx literals of
+                      Just literal -> [literalToString literal]  -- Convert Literal to String
+                      _ -> []
+        Nothing -> []
+    safeGet :: Int -> [a] -> Maybe a
+    safeGet idx xs
+      | idx < length xs = Just (xs !! idx)
+      | otherwise = Nothing
+    literalToString :: Literal -> String
+    literalToString (LiteralStr s) = s
+    literalToString (LiteralInt i) = show i
+    literalToString (LiteralBool b) = show b
 
 ---------------------------------------------------------------------------------------------------
 -- Alias Definitions
